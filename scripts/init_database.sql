b2631fd092eb27578c8f4c3112932b850883d6fc
Clement Callejon <clement.callejon@tutanota.com>
2026-08-31 17:39:27 +0200

    Initial commit

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+-- Script d'initialisation de la base MegaShop-B2B
+-- Auteur : [ClemCall]
+
+BEGIN;
+
+-- Décommentez et complétez :
+-- DROP TABLE IF EXISTS ... CASCADE;
+-- CREATE TABLE ...
+
+COMMIT;
+
