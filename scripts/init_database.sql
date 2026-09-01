64402f35d7456dd38d54a566a2cc2207921999e9
Clement Callejon <clement.callejon@tutanota.com>
2026-09-01 10:47:07 +0200

    feat(scripts): implement init_databse script

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,9 +3,39 @@
 
 BEGIN;
 
--- Décommentez et complétez :
--- DROP TABLE IF EXISTS ... CASCADE;
--- CREATE TABLE ...
+DROP TABLE IF EXISTS LIGNE_COMMANDE CASCADE;
+DROP TABLE IF EXISTS PRODUIT CASCADE;
+DROP TABLE IF EXISTS COMMANDE CASCADE;
+DROP TABLE IF EXISTS CLIENT CASCADE;
+
+CREATE TABLE CLIENT (
+  id_client UUID PRIMARY KEY,
+  nom VARCHAR(64) NOT NULL,
+  adresse VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE COMMANDE (
+  id_commande UUID PRIMARY KEY,
+  id_client UUID NOT NULL,
+  FOREIGN KEY (id_client) REFERENCES CLIENT(id_client),
+  date_achat DATE NOT NULL,
+  statut VARCHAR(16) NOT NULL CHECK(statut IN ('LIVREE', 'EN_COURS'))
+);
+
+CREATE TABLE PRODUIT (
+  id_produit UUID PRIMARY KEY,
+  designation VARCHAR(128) NOT NULL,
+  prix_unitaire_ht DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE LIGNE_COMMANDE (
+  id_commande UUID,
+  id_produit UUID,
+  FOREIGN KEY (id_commande) REFERENCES COMMANDE(id_commande),
+  FOREIGN KEY (id_produit) REFERENCES PRODUIT(id_produit),
+  quantite INT NOT NULL CHECK (quantite > 0),
+  PRIMARY KEY (id_commande, id_produit)
+);
 
 COMMIT;
 
